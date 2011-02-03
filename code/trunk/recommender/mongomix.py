6bf512182af45fe60bd32b13b93aee2bad4a15c4
Amir H. Hajizamani <ahh29@cam.ac.uk>
2011-02-03 07:39:14 +0000

    The recommender is now working and moderately well-engineered. Recommender working but very slow.

--- code/trunk/recommender/mongomix.py
+++ code/trunk/recommender/mongomix.py
@@ -2,8 +2,12 @@
 """A Python interface for accessing mixcloud data stored in a MongoDB."""
 
 import pymongo
+import random
+from collections import defaultdict
 
-REGISTRY_SIZE = 10
+REGISTRY_SIZE = 25
+MUTILATE_RATIO = 0.05
+SOC_SIM_COLLECTION = "soc_sim_cache"
 
 class MongoMixException(Exception):
     def __init__(self, *args):
@@ -12,8 +16,10 @@ class MongoMixException(Exception):
 
 class MixcloudDataset(object):
     """At the moment interfacing with one particular collection in a db, assumed
-    to be holding user data."""
-    def __init__(self, host, port, db, collection, type=None):
+    to be holding user data in the given collection."""
+    def __init__(self, 
+                 host="localhost", port=27017, 
+                 db="mixcloud", collection="user"):
         try:
             self.conn = pymongo.Connection(host=host,
                                            port=port)
@@ -22,48 +28,84 @@ class MixcloudDataset(object):
         finally:
             pass
         # To be one of "reference", "test", "training"
-        self.type = type
+        # self.type = type
         # Approxmiation of an LRU cache to keep track of recent MixcloudUser's
         self.cache = {}
+        self.similarity = Similarity(self)
         
     def _register_user(self, mcuser):
         """Register a MixcloudUser object with the Dataset instance."""
-        if len(self.registry) > REGISTRY_SIZE:
-            ejected = self.registry.popitem()
-        self.registry[mcuser["_id"]] = mcuser
+        if not isinstance(mcuser, MixcloudUser):
+            raise MongoMixException("User registration: not a MixcloudUser.")
+        if len(self.cache) > REGISTRY_SIZE:
+            ejected = random.choice(self.cache.keys())
+            ejected = self.cache.pop(ejected)
+###            print "INFO:cache:Ejected from cache:", ejected["_id"]
+        self.cache[mcuser["_id"]] = mcuser
+###        print "INFO:cache:Added to cache:", mcuser["_id"]
             
     def _fetch_user(self, id):
         """Fetch the user with the given id from the Mongo collection."""
-        return self.collection.find_one(id)
-        
+        data = self.collection.find_one(id)
+        if not data:
+            raise MongoMixException("_fetch_user: no such user found.", id)
+        return data
+
+    def _update_similarity_engine(self):
+        pass
+    
+    def cache_list(self):
+        return self.cache.keys()
+
     def get_user(self, id):
         """Returns a MixcloudUser instance containing the `id`'s data. Use cache
         (registry) if a recently accessed id, if not fetch and register it."""
-        if id in self.registry:
-            return self.registry[id]
+        if id in self.cache:
+            return self.cache[id]
         else:
-            user = MixcloudUser(self._fetch_user(id), origin=self)
+            user_data = self._fetch_user(id)
+            if not user_data:
+                return None
+            user = MixcloudUser(user_data, origin=self)
             self._register_user(user)
             return user
-    
+
+    def get_similarity(self, item1, item2):
+        return self.similarity.social_similarity(item1, item2)
+        
     def save_user(self, mcuser):
         """Write the contents of the MixcloudUser instance to the collection.
         Assumes a complete user document, so be careful!"""
-        if not isintance(mcuser, MixcloudUser):
-            return None
-        self.collection.save(mcuser.data, safe=True)
+        if not isinstance(mcuser, MixcloudUser):
+            raise MongoMixException("Could not save_user: not a MixcloudUser.")
+        self.collection.save(mcuser, safe=True)
     
-    def mutilate(self, ratio=0.05):
-        ### TODO
-        cursor = self.collection.find(spec={"following_count": {"$gt": 0},
-                                            "follower_count": {"$gt": 0}},
-                                      fields=["_id"])
+    def mutilate(self, ratio=MUTILATE_RATIO):
+        """Remove `ratio` proportion of outgoing links ("following") for all 
+        users. Users with "small enough" outlinks won't be affected.""" 
+        
+        # Any users with count values below this threshold won't be affected by
+        # the mutilation, so we can avoid processing them.
+        count_threshold = int(1/ratio) 
+        
+        # Find all users eligible for mutilation
+        eligible = self.collection.find(spec={
+                                              "following_count": 
+                                              {"$gt": count_threshold},
+                                              "follower_count": 
+                                              {"$gt": count_threshold}
+                                              },
+                                        fields=["_id"])
         
         victim_list = []
-        for each in cursor:
+        for each in eligible:
             victim_list.append(each["_id"])
-
-        
+            
+        while victim_list:
+            victim = self.get_user(random.choice(victim_list))
+            victim.mutilate_random_follows(ratio)
+            victim_list.remove(victim["_id"])
+                    
     def repair(self, reference):
         pass
 
@@ -74,55 +116,176 @@ class MixcloudDataset(object):
         pass
         
 
-class MixcloudUser(object):
+class MixcloudUser(dict):
     def __init__(self, data, origin):
-        self.data = data
-        # Origin is expect to be a MixcloudDataset instance
+        dict.__init__(self)
+        self.update(data)
         if not isinstance(origin, MixcloudDataset):
             raise MongoMixException("MixcloudUser init'ed with invalid origin.")
+        # Origin is expect to be a MixcloudDataset instance
         self.origin = origin
+
+    def __getattribute__(self, name):
+        """Try to get attribute as normal, then try dictionary lookup."""
+        try:
+            return dict.__getattribute__(self, name)
+        except AttributeError:
+            try:
+                return self[name]
+            except KeyError:
+                raise AttributeError()
         
     def save(self):
-        if self.origin:
-            self.origin.save_user(self)
-    
-    def get(self, field):
-        return self.data[field]
-    
+        self.origin.save_user(self)
+   
     def follow(self, user):
         target = self.origin.get_user(user)
-        if target["_id"] not in self.data["following"]:
-            self.data["following"].append(target["_id"])
-            self.data["following_count"] += 1
+        if target["_id"] not in self["following"]:
+            self["following"].append(target["_id"])
+            self["following_count"] += 1
             self.save()
-        if self.data["_id"] not in target["followers"]:
-            target.data["followers"].append(self.data["_id"])
-            target.data["follower_count"] += 1
+        if self["_id"] not in target["followers"]:
+            target["followers"].append(self["_id"])
+            target["follower_count"] += 1
             target.save()
         # Return the user just followed for further manipulation, if any
         return target
 
     def unfollow(self, user):
         target = self.origin.get_user(user)
-        if target["_id"] in self.data["following"]:
-            self.data["following"].remove(target["_id"])
-            self.data["following_count"] -= 1
+        if target["_id"] in self["following"]:
+            self["following"].remove(target["_id"])
+            self["following_count"] -= 1
             self.save()
-        if self.data["_id"] in target["followers"]:
-            target.data["followers"].remove(self.data["_id"])
-            target.data["follower_count"] -= 1
+        if self["_id"] in target["followers"]:
+            target["followers"].remove(self["_id"])
+            target["follower_count"] -= 1
             target.save()
         # Return the user just followed for further manipulation, if any
         return target
     
     def friend(self, user):
+        """Create social links in both directions."""
         new_friend = self.follow(user)
         new_friend.follow(self["_id"])
             
     def unfriend(self, user):
+        """Remove social links in both directions."""
         old_friend = self.unfollow(user)
         old_friend.unfollow(self["_id"])
 
+    def get_similarity(self, other_user):
+        return self.origin.get_similarity(self, other_user)
+
+    def mutilate_random_follows(self, ratio):
+        if ratio >= 1.0:
+            raise MongoMixException("MixcloudUser: mutilation ratio must be "
+                                    "less than 1.0.")        
+        outlinks = self.following()
+        pop_size = len(outlinks)
+        sample_size = int(pop_size*ratio)
+        # Take a random sample of the outlinks to be victims
+        victim_links = random.sample(outlinks, sample_size)
+        for each in victim_links:
+            self.unfollow(each)
+
+    def friends(self):
+        """Return the set followers who are also following."""
+        return (set(self["followers"]) & set(self["following"]))
+
     def neighbours(self):
-        return self.data["followers"] + self.data["following"]
+        """Return the set of all social links, followers or following."""
+        return set(self["followers"] + self["following"])
     
+    def social_hop(self, hops):
+        """Return dictionary of users in `hops` hops away in the network. 
+
+The dictionary keys are integers from 0 upwards, and the values are sets of 
+users reachable in as many hops."""
+        network = defaultdict(set)
+        network[0] = set((self._id,))   
+        for h in xrange(0, hops):
+            for each in network[h]:
+                current_user = self.origin.get_user(each)
+                network[h+1].update(current_user.neighbours() - network[h])                
+        return network                
+
+
+class Similarity(object):
+    def __init__(self, dataset):
+        if isinstance(dataset, MixcloudDataset):
+            self.ds = dataset        
+        self.soc_sim_alg = self.intersection_size
+        self._configure_cache()
+
+    def _configure_cache(self):
+        if  SOC_SIM_COLLECTION in self.ds.db.collection_names():
+            self.ss_cache = self.ds.db[SOC_SIM_COLLECTION]
+        else:
+            self.ss_cache = self.ds.db.create_collection(
+                                                         SOC_SIM_COLLECTION,
+                                                         size=(200*1024*1024),
+                                                         capped=True)
+            print "INFO: User similarity cache created."
+        
+    def _change_soc_sim_alg(self, alg_func):
+        self.soc_sim_alg = alg_func
+        self._clear_cache()
+        
+    def _clear_cache(self):
+        # drop cached values upon changing similarity metric 
+        self.ss_cache.remove(None)
+        
+    def get_cached_soc_sim(self, userid1, userid2):
+        found = None
+        for id in [(userid1+"-"+userid2), (userid2+"-"+userid1)]:            
+            found = self.ss_cache.find_one(id)
+            if found: 
+                return found["value"]
+        # After checking both possible locations for the cached value ...            
+        if not found:
+            return None
+    
+    def set_cached_soc_sim(self, userid1, userid2, value):
+        self.ss_cache.save({"_id": (userid1+"-"+userid2), "value": value})
+        
+    def social_similarity(self, user1, user2):
+        if not isinstance(user1, MixcloudUser):
+            user1 = self.ds.get_user(user1)
+        if not isinstance(user2, MixcloudUser):
+            user2 = self.ds.get_user(user2)
+        
+        cached_value = self.get_cached_soc_sim(user1._id, user2._id)
+        if cached_value:
+            print ("INFO:sim:cached similarity between "+user1._id+" and "+ 
+                   user2._id+" == "+str(cached_value)) 
+            return cached_value
+        else:
+            value = self.soc_sim_alg(user1, user2)
+            if value:
+                print ("INFO:sim:Calculated similarity between "+user1._id+
+                       " and "+ user2._id+" == "+str(value))                 
+                self.set_cached_soc_sim(user1._id, user2._id, value)
+            return value
+            
+    def intersection_size(self, user1, user2):
+        if (not user1) or (not user2):
+            return 0.0
+        if (not user1.following_count or not user2.following_count):
+            return 0.0
+        
+        user1_net = set(user1.following)
+        user2_net = set(user2.following)
+
+        return (len(user1_net & user2_net))
+        
+    def jaccard(self, user1, user2):
+        if (not user1) or (not user2):
+            return 0.0
+        user1_net = user1.neighbours()
+        user2_net = user2.neighbours()        
+        # Distance is equal to the number of common friends (intersection) 
+        # divided by the maximum number of common friends (min of sizes)
+        sim = float((len(user1_net & user2_net))) / (min(len(user1_net),
+                                                         len(user2_net))+1)        
+        return sim
--- code/trunk/recommender/recommender.py
+++ code/trunk/recommender/recommender.py
@@ -1,33 +1,47 @@
 #!/usr/bin/env python
 
-from similarity import Similarity
 from mongomix import MixcloudDataset
 from operator import itemgetter
+
+TOP_N = 10
 
 class RecommenderException(Exception):
     def __init__(self, *args):
         Exception.__init__(self, args)
 
-class Recommender:
+class Recommender(object):
     """"""
     def __init__(self, dataset):
         if isinstance(dataset, MixcloudDataset):
             self.ds = dataset
         else:
-            raise RecommenderException("Invalid Mixcloud Dataset.") 
-        self.sampler = Sampler(self.ds, 2)
-        self.similarity = Similarity(self.ds)
+            raise RecommenderException("Rec init: Invalid Mixcloud Dataset.") 
 
-    def recommend_social(self, user):
-        network = self.ds.get_user(user).get_social_graph(hops=2,
-                                                          exclude_existing=True)
+    def recommend_social(self, userid):
+        user = self.ds.get_user(userid)
+        network = user.social_hop(hops=2)
         recs = {}
-        for each in network:
-            recs[each] = self.similarity.social_similarity(user, each)
+        print "INFO:RECOMMENDER: friends of friends count:", len(network[2])
+        for each in network[2]:
+            sim_value = user.get_similarity(each)
+            if sim_value:
+                recs[each] = sim_value
         
         # See PEP-265 - return top similar users
-        return sorted(recs.iteritems(), key=itemgetter(1), reverse=True)[:10]
+        return sorted(recs.iteritems(), key=itemgetter(1), reverse=True)[:TOP_N]
     
-    def recommend_content(self, user):
+    def recommend_content(self, userid):
         pass
+    
+if __name__ == "__main__":
+    import sys
+    userid = sys.argv[1]
+    ds = MixcloudDataset(host="nigela")
+    rec = Recommender(ds)
+    try:
+        results = rec.recommend_social(userid)
+    finally:
+        pass
+    for user, value in results:
+        print user, ":", value
     
