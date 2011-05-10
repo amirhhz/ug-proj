ffc926fc86405611020485dcecd3ec7168d23081
Amir H. Hajizamani <ahh29@cam.ac.uk>
2011-05-10 09:43:43 +0000

    quick commit. Recommender has a logger.

--- code/trunk/recommender/recommender.py
+++ code/trunk/recommender/recommender.py
@@ -2,6 +2,11 @@
 
 from mongomix import MixcloudDataset
 from operator import itemgetter
+import logging
+
+reclog = logging.getLogger(__name__)
+reclog.setLevel(logging.DEBUG)
+reclog.addHandler(logging.StreamHandler())
 
 TOP_N = 50
 
@@ -21,7 +26,7 @@ class Recommender(object):
     def recommend_social(self, userid):
         user = self.ds.get_user(userid)
         network = user.fofs()
-        print "INFO:RECOMMENDER:",userid,"friends of friends count:", len(network)
+        reclog.info("rec_soc:{0} friends of friends count: {1}".format(userid, len(network)))
         recs = {}
         for each in network:
             sim_value = user.get_social_similarity(each)
@@ -29,9 +34,9 @@ class Recommender(object):
                 recs[each] = sim_value        
         # See PEP-265 - return top similar users
         return sorted(recs.iteritems(), key=itemgetter(1), reverse=True)[:50]
-    
-    def recommend_content(self, userid):
-        pass
+
+    def change_soc_sim_alg(self, alg_name):
+        self.ds.similarity._change_soc_sim_alg(alg_name)
 
     
 class Evaluator(object):
@@ -52,18 +57,22 @@ class Evaluator(object):
         per_user_measures = {}
         for item in test_user_list:
             results = self.rec.recommend_social(item)                
+
             max_positives = (
                 set(self.ref._get_user_follows(item)) - 
                 set(self.test._get_user_follows(item)))
-            
+
             true_positives = set()
             for name, value in results:
                 if name in max_positives:
                     true_positives.add(name)
-            
-            precision = float(len(true_positives))/len(results)
-            recall = float(len(true_positives))/len(max_positives)
-            
+
+            precision = 0
+            recall = 0 
+            if max_positives and true_positives:
+                precision = float(len(true_positives))/len(results)
+                recall = float(len(true_positives))/len(max_positives)
+
             per_user_measures[item] = {
                 "precision": precision,
                 "recall": recall,
